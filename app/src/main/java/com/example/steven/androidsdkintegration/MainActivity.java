2652f93255e1b8967c924f48486d56e1bf675a8c
Steven van Stiphout <steven.vanstiphout@deltadna.com>
2016-03-22 19:14:45 +0000

    switched to dev distribution

--- app/src/main/java/com/example/steven/androidsdkintegration/MainActivity.java
+++ app/src/main/java/com/example/steven/androidsdkintegration/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import com.deltadna.android.sdk.DDNA;
 import com.deltadna.android.sdk.Event;
+import com.deltadna.android.sdk.Product;
+import com.deltadna.android.sdk.Transaction;
 
 public class MainActivity extends AppCompatActivity {
     String TAG = "MainActivity";
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         final TextView textLabel = (TextView) findViewById(R.id.textLabel);
         Button startSdkButton = (Button) findViewById(R.id.StartSDK);
         Button simpleEventButton = (Button) findViewById(R.id.simpleEvent);
+        Button complexEventButton = (Button) findViewById(R.id.complexEvent);
         Button upLoadEventsButton = (Button) findViewById(R.id.uploadEvents);
         Button newSessionButton = (Button) findViewById(R.id.newSession);
 
@@ -40,12 +43,28 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "record simple event");
                 DDNA.instance().recordEvent(
                         new Event("options")
-                        .putParam("action","click")
-                        .putParam("option","uselessButton")
+                                .putParam("action", "click")
+                                .putParam("option", "uselessButton")
                 );
                 textLabel.setText("options event recorded");
             }
+        });
 
+        complexEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "record complex event");
+                DDNA.instance().recordEvent(new Transaction(
+                                "IAP an in app purchase",
+                                "PURCHASE",
+                                new Product().addItem("Aragorns sword", "sword", 1)
+                                            .addItem("Legolas bow","bow",1)
+                                            .addItem("Gimilis axe","axe",1)
+                                            .addVirtualCurrency("Golden ring","PREMIUM",1),
+                                new Product().setRealCurrency("USD", 100))
+                );
+                textLabel.setText("transaction event recorded");
+            }
         });
 
         upLoadEventsButton.setOnClickListener(new View.OnClickListener() {
@@ -63,11 +82,10 @@ public class MainActivity extends AppCompatActivity {
                 String oldSessionId = DDNA.instance().getSessionId();
                 DDNA.instance().newSession();
                 String newSessionId = DDNA.instance().getSessionId();
-                Log.d(TAG, "New session started, old sessionID = "+oldSessionId+" new sessionID: "+newSessionId);
-                textLabel.setText("new sessionId:"+newSessionId);
+                Log.d(TAG, "New session started, old sessionID = " + oldSessionId + " new sessionID: " + newSessionId);
+                textLabel.setText("new sessionId:" + newSessionId);
             }
         });
-
     }
 
     @Override
